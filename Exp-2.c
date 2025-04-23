c1895bbe28d9a6df93a95ed700b5e9308d33ebc3
RishabhYadav-Dev <rishabh18yad@gmail.com>
2025-04-23 20:18:13 +0530

    Update Exp-2.c

--- Exp-2.c
+++ Exp-2.c
@@ -3,8 +3,10 @@
 void selectionSort(int arr[], int n)
 { 
 int min, i , j;
-for(i = 0; i < n-1; i++){ min = i;
-for(j = i+1; j < n; j++){ if(arr[j] < arr[min]){
+for(i = 0; i < n-1; i++){
+min = i;
+for(j = i+1; j < n; j++){
+if(arr[j] < arr[min]){
 min = j;
 }
 }

c87ae4e8d5e3e075a0948dc54c0b8fa58c7fd3d1
RishabhYadav-Devloper <rishabh18yad@gmail.com>
2025-04-23 19:24:31 +0530

    Update Exp-4.c

--- Exp-4.c
+++ Exp-4.c
@@ -28,3 +28,18 @@ printf("\n"); return 0;
 
 
 Algo
+QUICKSORT(A, p,r) 
+1 if p<r 
+2 q = PARTITION(A, p,r) 
+3 QUICKSORT(A, p,q - 1) 
+4 QUICKSORT(A, q + 1,r)
+
+           PARTITION (A. p,r) 
+1 x = A[r] 
+2 i = p - 1 
+3 for j = ptor-1 
+4 if A[j] ≤ x 
+5 i = i + 1 
+6 exchange A[i] with A[j] 
+7 exchange A[i + 1] with A[r] 
+8 return i + 1

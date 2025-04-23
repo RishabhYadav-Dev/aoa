794c849f9d9c2cc427b8031c88e945dcca05b10a
RishabhYadav-Devloper <rishabh18yad@gmail.com>
2025-04-23 17:05:51 +0530

    Update Exp-1.c

--- Exp-1.c
+++ Exp-1.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
-
-void selectionSort(int arr[], int n){ int min, i , j;
-for(i = 0; i < n-1; i++){ min = i;
-for(j = i+1; j < n; j++){ if(arr[j] < arr[min]){
-min = j;
+void insertion_sort(int arr[], int n) { for (int i = 0; i < n; i++) {
+int j = i;
+while (j > 0 && arr[j - 1] > arr[j]) { int temp = arr[j - 1];
+arr[j - 1] = arr[j]; arr[j] = temp;
+j--;
 }
 }
-int temp = arr[i]; arr[i] = arr[min]; arr[min] = temp;
-}
-}
-
-int main() { int n;
-printf("Enter the number of Elements: "); 
-scanf("%d", &n);
-int arr[n];
-printf("Array input: "); 
-for(int i = 0; i < n; i++){ 
-scanf("%d", &arr[i]);
-}
-selectionSort(arr , n);
-printf("Resulted Sorted array using Selection Sort Approach: "); for(int i = 0; i < n; i++){
+printf("After Using insertion sort: "); for (int i = 0; i < n; i++) {
 printf("%d ", arr[i]);
 }
-return 0;
+printf("\n");
+}
+int main() { int n;
+printf("Enter the number of Elements: "); scanf("%d", &n);
+int arr[n];
+printf("Array input: "); for(int i = 0; i < n; i++){ scanf("%d", &arr[i]);
+}
+insertion_sort(arr , n); return 0;
 }
 
 

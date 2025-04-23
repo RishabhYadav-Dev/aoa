8f274b8b1e3d263b6d964bdd66765208a0240345
RishabhYadav-Dev <rishabh18yad@gmail.com>
2025-04-23 20:16:05 +0530

    Update Exp-2.c

--- Exp-2.c
+++ Exp-2.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 
-void selectionSort(int arr[], int n){ int min, i , j;
+void selectionSort(int arr[], int n)
+{ 
+int min, i , j;
 for(i = 0; i < n-1; i++){ min = i;
 for(j = i+1; j < n; j++){ if(arr[j] < arr[min]){
 min = j;
 }
 }
-int temp = arr[i]; arr[i] = arr[min]; arr[min] = temp;
+int temp = arr[i]; arr[i] = arr[min]; 
+arr[min] = temp;
 }
 }
 
 int main() { int n;
-printf("Enter the number of Elements: "); scanf("%d", &n);
+printf("Enter the number of Elements: "); 
+scanf("%d", &n);
 int arr[n];
-printf("Array input: "); for(int i = 0; i < n; i++){ scanf("%d", &arr[i]);
+printf("Array input: "); for(int i = 0; i < n; i++)
+{ 
+scanf("%d", &arr[i]);
 }
 selectionSort(arr , n);
-printf("Resulted Sorted array using Selection Sort Approach: "); for(int i = 0; i < n; i++){
+printf("Resulted Sorted array using Selection Sort Approach: "); 
+for(int i = 0; i < n; i++){
 printf("%d ", arr[i]);
 }
 return 0;
